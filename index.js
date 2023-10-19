d388593e1a2d60f3705fc023b33d89afc2acd288
EnderChamp_ <147697938+EnderCha@users.noreply.github.com>
2023-10-19 13:39:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
-const config = require('./config.json');
 
-const {loadEvents} = require('./Handlers/eventHandler')
-
-const client = new Client({
-    intents: [Object.keys(GatewayIntentBits)],
-    partials: [Object.keys(Partials)],
-});
-
-client.setMaxListeners(0);
-
-client.login(config.token).then(() => {
-    console.log('El bot esta online');
-});
